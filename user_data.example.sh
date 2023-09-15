093bfc0b1ec06d54adc77c4b92089fce49eabb14
Victor Queiroga <victorqueiroga@gmail.com>
2023-09-14 23:08:48 -0300

    user data

--- user_data.example.sh
+++ user_data.example.sh
@@ -119,7 +119,7 @@ sudo chown -R www-data:www-data ${HUMHUB_DIR}
 
 if [ ! -f '~/.passwd-s3fs' ]; then
   echo "$HUMHUB_AWS_KEY_ID:$HUMHUB_AWS_ACCESS_KEY" >~/.passwd-s3fs
-  sudo chown ubuntu:ubuntu /etc/passwd-s3fs
+  sudo chown ubuntu:ubuntu ~/.passwd-s3fs
   chmod 600 ~/.passwd-s3fs
   sudo sed -i '/^#user_allow_other/s/^#//' /etc/fuse.conf
 fi
